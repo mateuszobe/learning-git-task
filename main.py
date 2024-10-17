2a326bfc9bc6466973c0ccaafbcb5c0ab682dc95
Mateusz Oberski <mateuszoberski2@gmail.com>
2024-10-17 14:24:39 +0200

    Add loop

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ print("Lista zakupów")
 slownik =  {"piekarnia":["chleb","bułki","pączek"],
             "warzywniak":["marchew","seler","rukola"]}
 x = 0
+for sklep, rzeczy in slownik.items():
+    sklep = sklep.title()
+    rzeczy = [rzecz.title() for rzecz in rzeczy]
+    x +=len(rzeczy)
+    print (f"Idę do {sklep}, kupuję tu następujące rzeczy: {rzeczy}")
