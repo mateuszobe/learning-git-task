f2720a38cef23f0a7f462f517f448be24dc89da6
Mateusz Oberski <mateuszoberski2@gmail.com>
2024-10-22 14:10:51 +0200

    Add Zadanie 1

--- file_1.py
+++ file_1.py
@@ -1,2 +1,15 @@
 print("zadanie 3.4")
-#komentarz 
+
+print("Zadanie 1")
+print()
+print("Lista zakupów")
+
+slownik =  {"piekarnia":["chleb","bułki","pączek"],
+            "warzywniak":["marchew","seler","rukola"]}
+x = 0
+for sklep, rzeczy in slownik.items():
+    sklep = sklep.title()
+    rzeczy = [rzecz.title() for rzecz in rzeczy]
+    x +=len(rzeczy)
+    print (f"Idę do {sklep}, kupuję tu następujące rzeczy: {rzeczy}")
+print(f"W sumie kupuję {x} produktów")
